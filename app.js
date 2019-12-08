fc385de3458848fd0e4b8dd88b24a454a9dae0f2
prlabu <1thisisfornothing1@gmail.com>
2019-12-08 00:10:26 -0600

    absolute file name changed to dynamic name

--- app.js
+++ app.js
@@ -109,7 +109,7 @@ app.post('/upload', multer.single('file'), (req, res, next) => {
       + `https://storage.googleapis.com/${bucket.name}/${blob.name}`
       + '&& /home/vagrant/launcher/opensmileSad.sh data/new_files'
       + '&& /home/vagrant/launcher/diartk.sh data/new_files/ opensmileSad'
-      + '&& python /vagrant/utils_custom/rttm_converter.py /vagrant/data/new_files/diartk_opensmileSad_TOW-toy-data.rttm /vagrant/data/new_files/stats.json'
+      + `&& python /vagrant/utils_custom/rttm_converter.py /vagrant/data/new_files/diartk_opensmileSad_${blob.name.slice(0, blob.name.length - 4)}.rttm /vagrant/data/new_files/stats.json`
 
       ssh.execCommand(cmd).then(function(result) {
           // + '&& wget -P /vagrant/data/new_new_files https://storage.googleapis.com/tugbucket1234/TOW-toy-data.wav'
@@ -128,7 +128,7 @@ app.post('/upload', multer.single('file'), (req, res, next) => {
             console.log(error)
             res.render('error.pug');
           })
-          
+
       })
 
     });
